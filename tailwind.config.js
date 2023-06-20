a2d551503f81ce195d1184785b602fae0f6c60ac
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-06-20 06:35:15 -0500

    initial setup complete. Created empty Navbar component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,21 @@
-const { fontFamily } = require("tailwindcss/defaultTheme");
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        mont: ["var(--font-mont)", ...fontFamily.sans],
-      },
+export const content = [
+  // Or if using `src` directory:
+  "./src/**/*.{js,ts,jsx,tsx,mdx}",
+];
+export const theme = {
+  extend: {
+    fontFamily: {
+      mont: ["var(--font-mont)", ...fontFamily.sans],
+    },
+    colors: {
+      dark: "#1b1b1b",
+      light: "#f5f5f5",
+      primary: "#B63E96",
+      primaryDark: "#58E6D9", // 80,230,217
     },
   },
-  plugins: [],
 };
+export const plugins = [];
