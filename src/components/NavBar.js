99174302898490ab8a8c6f31553dc0991304a1c3
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-06-24 11:07:29 -0500

    updated the Navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,9 +9,15 @@ const CustomLink = ({ href, title, className = "" }) => {
   const router = useRouter();
 
   return (
-    <Link href={href} className={`${className} relative group`}>
+    <Link
+      href={href}
+      className={`${className} relative group ${
+        router.asPath === href ? "font-bold" : "font-semibold"
+      }`}
+    >
       {title}
 
+      {/* The below span contains the underlines on the nav bar */}
       <span
         className={`h-[1.2px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300
       ${router.asPath === href ? "w-full" : "w-0"}
@@ -27,11 +33,11 @@ const NavBar = () => {
   return (
     <header className="w-full sticky top-0 bg-glossy px-32 py-2 font-medium flex items-center justify-between z-10 pb-0 drop-shadow-md">
       {/* LEFT SIDE */}
-      <nav className="flex-1/3">
+      <nav>
         <CustomLink href="/" title="Home" className="mr-4" />
-        <CustomLink href="/page" title="Public Skating" className="mx-4" />
-        <CustomLink href="/page" title="Hockey" className="mx-4" />
-        <CustomLink href="/page" title="Groups" className="ml-4" />
+        <CustomLink href="/skating" title="Public Skating" className="mx-4" />
+        <CustomLink href="/hockey" title="Ice Hockey" className="mx-4" />
+        <CustomLink href="/events" title="Events" className="ml-4" />
       </nav>
 
       <div className="left-[50%] top-2 translate-x-[-50%]">
